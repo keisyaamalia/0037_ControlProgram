6f75588341cfcb176d7d827912f98ee1de3b39d1
keisyaamalia <kkaysaamalia@gmail.com>
2026-03-09 11:40:30 +0700

    rerata, if, status dan cout

--- nested_if.cpp
+++ nested_if.cpp
@@ -23,3 +23,13 @@ int main()
     cin >> nilBI;
     cout << "Masukkan nilai Matemetika = ";
     cin >> nilMT;
+
+    // rerata = (nilBI+nilMT)/2;
+    // if (rerata >= 60)
+    // status = "Lolos";
+    // else
+    // status = "Gagal";
+    rerata = Rata_rata(nilBI, nilMT);
+    // status = Status_Lulus(rerata);
+    cout << "Status kelulusan = " << Status_lulus(rerata) << " ,dengan nilai rerata = " << rerata << endl;
+}
